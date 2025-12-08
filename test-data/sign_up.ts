3ded94f85a1915553b2a265d897dff7c3be53138
Ahmed Madyan <ahmed.madyan01@gmail.com>
2025-12-09 00:52:08 +0300

    Create sign_up.ts

---
+++ test-data/sign_up.ts
@@ -0,0 +1,12 @@
+export default {
+    "name": "Test User",
+    "password": "invalidP@ss",
+    "first_name": "Test",
+    "last_name": "User",
+    "address": "123 Test St, Test City, TS 12345",
+    "country": "Testland",
+    "state": "Test State",
+    "city": "Test City",
+    "zip_code": "12345",
+    "mobile_number": "+1234567890"
+}
