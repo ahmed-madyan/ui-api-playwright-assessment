b4c6ae524deb7ab10400fc0bf578ac769c199fc9
Ahmed Madyan <ahmed.madyan01@gmail.com>
2025-12-09 00:52:33 +0300

    Update login.spec.ts

--- tests/ui/specs/login.spec.ts
+++ tests/ui/specs/login.spec.ts
@@ -1,19 +1,20 @@
 import { test } from '../fixtures/login-fixture';
 import tsData from '../../../test-data/test-users';
 
-test.describe('[Login Feature] @ui',() =>{
-//====================Tests======================
-    test('Validate valid login @ui', async ( {homePage,loginPage} ) => {
-        await loginPage.open();
-        await homePage.navigateToSignUpPage();
-        await loginPage.login(tsData.valid_email, tsData.valid_password);
+//=======================Group Test==========================
+test.describe('[Login Feature] @ui', () => {
+    //====================Tests======================
+    test('Validate valid login @ui', async ({ homePage, loginPage }) => {
+        await loginPage.navigateToURL();
+        await homePage.navigateToSignInSignUpPage();
+        await loginPage.login(tsData.credentials.valid.email, tsData.credentials.valid.password);
         await homePage.assertLogoutBtn();
     });
 
-    test('Validate in-valid login @ui', async ( {homePage,loginPage} ) => {
-        await loginPage.open();
-        await homePage.navigateToSignUpPage();
-        await loginPage.login(tsData.invalid_email, tsData.invalid_password);
+    test('Validate in-valid login @ui', async ({ homePage, loginPage }) => {
+        await loginPage.navigateToURL();
+        await homePage.navigateToSignInSignUpPage();
+        await loginPage.login(tsData.credentials.invalid.email, tsData.credentials.invalid.password);
         await loginPage.assertInvalidLoginMessage();
     });
 });
