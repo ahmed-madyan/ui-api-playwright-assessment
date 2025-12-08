78a40ebc9185ba7cb587c4e376ad81b25e205f54
Ahmed Madyan <ahmed.madyan01@gmail.com>
2025-12-09 00:52:18 +0300

    Update users-test.spec.ts

--- tests/api/specs/users-test.spec.ts
+++ tests/api/specs/users-test.spec.ts
@@ -4,9 +4,9 @@ import usersRequest from '../endpoints/users-endpoints';
 let response;
 let jsonResponse;
 //========================Tests=============================
-test.describe('Users API test @api',() =>{
+test.describe('Users API test @api', () => {
     //-----------------------get 200------------------------
-    test('Check get users response success response', async ({request}) => {
+    test('Check get users response success response', async ({ request }) => {
         response = await usersRequest.getUsers(request);
         jsonResponse = await response.json();
         //assert on status code
@@ -15,21 +15,21 @@ test.describe('Users API test @api',() =>{
         await expect(jsonResponse.length).toBe(100);
     });
     //----------------------get with params 200---------------
-    test('Check get users response for a specific user', async ({request}) => {
+    test('Check get users response for a specific user', async ({ request }) => {
         response = await usersRequest.getUser2(request);
         jsonResponse = await response.json();
         console.log(jsonResponse);
         await expect(jsonResponse[0].title).toEqual('qui est esse');
     });
     //-----------------get response headers 200---------------
-    test('Check get users response header', async ({request}) => {
+    test('Check get users response header', async ({ request }) => {
         response = await usersRequest.getUsers(request);
         const headers = await response.headers();
         console.log(headers);
         await expect(headers.connection).toEqual('keep-alive');
     });
     //-----------------------post 201-------------------------
-    test('Check post user response status code and body', async ({request}) => {
+    test('Check post user response status code and body', async ({ request }) => {
         response = await usersRequest.createUser(request);
         jsonResponse = await response.json();
         console.log(jsonResponse);
