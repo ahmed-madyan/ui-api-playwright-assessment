52701794ae9874379a5a03cfe736d13982c9f836
Ahmed Madyan <ahmed.madyan01@gmail.com>
2025-12-09 12:05:03 +0300

    Update sign_up.spec.ts

--- tests/ui/specs/sign_up.spec.ts
+++ tests/ui/specs/sign_up.spec.ts
@@ -1,22 +1,27 @@
-import { test } from '../fixtures/login-fixture';
-import tsData from '../../../test-data/test-users';
+import { test } from '../fixtures/signUp-fixture';
+import tsData from '../../../test-data/sign_up';
 //=======================Variables==========================
 let email: string;
-let password: string;
-
 //=======================Group Test==========================
 test.describe('[Sign-Up Feature] @ui', () => {
     //====================Tests======================
-    test('Validate Sign-Up @ui', async ({ homePage, loginPage }) => {
+    test('[Sign-Up][UI][TC01][E2E] Validate login with signed up user @ui', async ({ homePage, loginPage, signUp }) => {
         const randomNumber: number = Math.random();
         email = ("test" + randomNumber + "@gmail.com");
-        password = ("test@" + randomNumber);
         await loginPage.navigateToURL();
         await homePage.navigateToSignInSignUpPage();
-    });
-
-    test('Validate login with signed up user @ui', async ({ homePage, loginPage }) => {
+        await homePage.navigateToSignUpPage(tsData.name, email);
+        await signUp.signUpWithValidData(
+            tsData.password, tsData.birth_day, tsData.birth_month, tsData.birth_year,
+            tsData.first_name, tsData.last_name, tsData.address, tsData.country, tsData.state, tsData.city,
+            tsData.zip_code, tsData.mobile_number);
+        await signUp.verifyAccountCreatedMessageIsVisible();
+        await signUp.verifyAccountCreatedCongratsMessageIsVisible();
+        await signUp.verifyAccountCreatedInfoMessageIsVisible();
         await loginPage.navigateToURL();
+        await homePage.logout();
         await homePage.navigateToSignInSignUpPage();
+        await loginPage.login(email, tsData.password);
+        await homePage.assertLogoutBtn();
     });
 });
